0364688088ee70bda01a4bdfacf4764d09bd3bb6
Carlos Salazar Moreira <56647144+carfesal@users.noreply.github.com>
2022-01-16 23:18:27 -0500

    Cambios ads

--- Services/ActiveDirectoryService.cs
+++ Services/ActiveDirectoryService.cs
@@ -228,7 +228,7 @@ namespace kairosApp.Services
 
         public bool CreateUser(ADCreateUser user)
         {
-            // Creating the PrincipalContext
+            /*// Creating the PrincipalContext
             PrincipalContext principalContext = null;
             try
             {
@@ -249,7 +249,7 @@ namespace kairosApp.Services
             }
 
             // Create the new UserPrincipal object
-            UserPrincipal userPrincipal = new UserPrincipal(principalContext);
+            UserPrincipal userPrincipal = new UserPrincipal(principalContext);*/
             /*
             if (lastName != null && lastName.Length > 0)
                 userPrincipal.Surname = lastName;
@@ -264,7 +264,7 @@ namespace kairosApp.Services
                 userPrincipal.EmailAddress = emailAddress;
 
             if (telephone != null && telephone.Length > 0)
-                userPrincipal.VoiceTelephoneNumber = telephone;*/
+                userPrincipal.VoiceTelephoneNumber = telephone;
 
             if (user.Username != null && user.Username.Length > 0)
                 userPrincipal.SamAccountName = user.Username;
@@ -283,6 +283,22 @@ namespace kairosApp.Services
             catch (Exception e)
             {
                 Debug.WriteLine("Exception creating user object. " + e);
+                return false;
+            }*/
+            DirectoryEntry ouEntry = new DirectoryEntry("LDAP://192.168.253.3", "buscador", "T3st*12$");
+
+            try
+            {
+                DirectoryEntry childEntry = ouEntry.Children.Add("CN="+user.Username, "user");
+                childEntry.CommitChanges();
+                ouEntry.CommitChanges();
+                childEntry.Invoke("SetPassword", new object[] { "HOLIWIS" });
+                childEntry.CommitChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
                 return false;
             }
         }
